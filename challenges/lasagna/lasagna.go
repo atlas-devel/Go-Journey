0af976b3829bc613505ed67612ecbb62849df29c
Leon <irakaramale@gmail.com>
2026-09-17 14:53:36 +0300

    add remainingOverTime function

---
+++ challenges/lasagna/lasagna.go
@@ -0,0 +1,11 @@
+package main
+
+const OverTime = 40
+
+func RemainingOverTime(actual int) int {
+	return OverTime - actual
+}
+func main() {
+	var remaining = RemainingOverTime(20)
+	println(remaining)
+}

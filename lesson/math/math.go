592f955ac053ea8cb9794b0212987e6acb68995c
Leon <irakaramale@gmail.com>
2026-09-20 19:49:23 +0300

    create math module

--- lesson/math/math.go
+++ lesson/math/math.go
@@ -5,11 +5,11 @@ import (
 	"math"
 )
 
-func main(){
+func mathOperatons(){
 	fmt.Println(math.Min(2,3))
 	fmt.Println(math.Max(2,3))
 	fmt.Println(math.Sqrt(4))
 	fmt.Println(math.Pow(3,2))
 	fmt.Println(math.Round(3.9321))
-
+	
 }

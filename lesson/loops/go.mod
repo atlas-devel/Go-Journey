fed9f2fffd90905de15bea3ce96e36e767b58593
Leon <irakaramale@gmail.com>
2026-09-24 13:12:34 +0300

    lesson: implement for loop

---
+++ lesson/loops/go.mod
@@ -0,0 +1,3 @@
+module github.com/atlas-devel/Go-Journey/loops
+
+go 1.26.5
---
+++ lesson/loops/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	forloop "github.com/atlas-devel/Go-Journey/loops/for-loop"
+)
+
+func main() {
+	forloop.ForLoop()
+
+}

2c0efb297953b44ec3d284766f8a6c48ac14d093
Leon <irakaramale@gmail.com>
2026-09-26 13:35:16 +0300

    lesson: while loop

--- lesson/loops/main.go
+++ lesson/loops/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	forloop "github.com/atlas-devel/Go-Journey/loops/for-loop"
+	"github.com/atlas-devel/Go-Journey/loops/while"
 )
 
 func main() {
 	forloop.ForLoop()
+	while.WhileLoop()
 
 }
---
+++ lesson/loops/while/while.go
@@ -0,0 +1,14 @@
+package while
+
+import (
+	"fmt"
+)
+
+func WhileLoop() {
+x:=20
+for (x>0){
+	fmt.Println(x)
+	x--
+}
+ 
+}
